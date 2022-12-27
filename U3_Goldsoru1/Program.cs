95dc320fc4d9aeb7f6d1b195b4a60157cdafb8b8
BIL-LAB5PC09\Ogrenci <123@123.COM>
2022-12-27 16:13:42 +0300

    no message

---
+++ U3_Goldsoru1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_Goldsoru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double cevre, alan, r;
+            Console.Write("Yarıçapı Gir : ");
+            r = Convert.ToDouble(Console.ReadLine());
+            cevre = 2 * pi * r;
+            alan = pi * r * r;
+            Console.WriteLine("===============================");
+            Console.WriteLine("Alan: " + alan);
+            Console.WriteLine("Çevre: " + cevre);
+            Console.ReadKey();
+        }
+    }
+    class Daire
+    {
+        private int a, b;
+        public Daire(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
+// İLK BEN YAPTIIIIM ş
---
+++ U3_S77/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S77
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen(6, 9);
+
+            Console.WriteLine("Diktörtgenin alanı :" + dikdortgen.AlanHesapla());
+            Console.WriteLine("Diktörtgenin cevresi :" + dikdortgen.CevreHesapla());
+
+            Console.ReadLine();
+        }
+    }
+    class Dikdortgen
+    {
+        private int a, b;
+        public Dikdortgen(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int AlanHesapla()
+        {
+            return a * b;
+        }
+        public int CevreHesapla()
+        {
+            return 2 * (a + b);
+        }
+    }
+}
