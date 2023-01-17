9a68b45adff3451ac46988132288610949a7439a
BIL-LAB5PC08\Ogrenci <123@123.com>
2023-01-17 16:36:20 +0300

    no message

---
+++ U3_SS_108/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_SS_108
+{
+    interface IKütüphane
+    {
+        void Oku();
+    }
+    public class Kitap : IKütüphane
+    {
+        public void Oku()
+        {
+            Console.WriteLine("kitap Oku");
+        }
+    }
+    public class Dergi : IKütüphane
+    {
+        public void Oku()
+        {
+            Console.WriteLine("Dergi Oku");
+        }
+    }
+            public class Ansiklopedi : IKütüphane
+            {
+                public void Oku()
+                {
+                    Console.WriteLine("Ansiklopedi Oku");
+                }
+            }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Kitap kitap = new Kitap();
+                    kitap.Oku();
+
+
+                Dergi dergi = new Dergi();
+                    dergi.Oku();
+
+
+                Ansiklopedi ansiklopedi = new Ansiklopedi();
+                    ansiklopedi.Oku();
+
+
+                    Console.WriteLine("=======================");
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ U3_S_109/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_109
+{
+    interface ISekil
+    {
+        void Ciz();
+    }
+    class Kare : ISekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("kare çiz");
+        }
+    }
+    class Daire : ISekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("daire çiz");
+        }
+    }
+    class Ucken : ISekil
+    {
+        public void Ciz()
+        {
+            Console.WriteLine("üçken çiz");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<sekil> sekiller = new List<sekil>
+            {
+                new Daire(),
+                new Kare(),
+                new Ucken()
+            };
+            foreach (var sekil in sekiller)
+            {
+                sekil.Ciz();
+            }
+        }
+    }
+}
