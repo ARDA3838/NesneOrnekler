ad91899078dfc29687b3dfae60c88916f48a6669
SBEML\amp-1833 <123@gmail.com>
2023-02-21 14:48:43 +0300

    no message

---
+++ U3_S_115/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U3_S_115
+{
+    enum Seviyeler
+    {
+        çok_düşük, // 0
+        düşük, // 1
+        orta, // 2
+        yüksek, //3
+        çpk_yüksek // 4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Seviyeler.düşük);
+            Console.WriteLine((int)Seviyeler.düşük);
+            Console.ReadLine();
+        }
+    }
+}
