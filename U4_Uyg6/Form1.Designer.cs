c8c29cbf6d158e59591a1369a9f4af890b685000
BIL-LAB1PC01\Ogrenci <123@123.COM>
2023-02-28 16:08:21 +0300

    no message

--- U4_Uyg6/Form1.Designer.cs
+++ U4_Uyg6/Form1.Designer.cs
@@ -29,13 +29,145 @@ namespace U4_Uyg6
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_adSoy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_yazılı1 = new System.Windows.Forms.TextBox();
+            this.textBox_yazılı2 = new System.Windows.Forms.TextBox();
+            this.textBox_sözlü2 = new System.Windows.Forms.TextBox();
+            this.textBox_sözlü1 = new System.Windows.Forms.TextBox();
+            this.button_ekle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD SOYAD";
+            // 
+            // textBox_adSoy
+            // 
+            this.textBox_adSoy.Location = new System.Drawing.Point(83, 21);
+            this.textBox_adSoy.Name = "textBox_adSoy";
+            this.textBox_adSoy.Size = new System.Drawing.Size(148, 20);
+            this.textBox_adSoy.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "YAZILI 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "YAZILI 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SÖZLÜ 2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(190, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "SÖZLÜ 1";
+            // 
+            // textBox_yazılı1
+            // 
+            this.textBox_yazılı1.Location = new System.Drawing.Point(18, 133);
+            this.textBox_yazılı1.Name = "textBox_yazılı1";
+            this.textBox_yazılı1.Size = new System.Drawing.Size(49, 20);
+            this.textBox_yazılı1.TabIndex = 6;
+            // 
+            // textBox_yazılı2
+            // 
+            this.textBox_yazılı2.Location = new System.Drawing.Point(104, 133);
+            this.textBox_yazılı2.Name = "textBox_yazılı2";
+            this.textBox_yazılı2.Size = new System.Drawing.Size(49, 20);
+            this.textBox_yazılı2.TabIndex = 7;
+            // 
+            // textBox_sözlü2
+            // 
+            this.textBox_sözlü2.Location = new System.Drawing.Point(279, 133);
+            this.textBox_sözlü2.Name = "textBox_sözlü2";
+            this.textBox_sözlü2.Size = new System.Drawing.Size(49, 20);
+            this.textBox_sözlü2.TabIndex = 9;
+            // 
+            // textBox_sözlü1
+            // 
+            this.textBox_sözlü1.Location = new System.Drawing.Point(193, 133);
+            this.textBox_sözlü1.Name = "textBox_sözlü1";
+            this.textBox_sözlü1.Size = new System.Drawing.Size(49, 20);
+            this.textBox_sözlü1.TabIndex = 8;
+            // 
+            // button_ekle
+            // 
+            this.button_ekle.Location = new System.Drawing.Point(266, 18);
+            this.button_ekle.Name = "button_ekle";
+            this.button_ekle.Size = new System.Drawing.Size(62, 61);
+            this.button_ekle.TabIndex = 10;
+            this.button_ekle.Text = "EKLE";
+            this.button_ekle.UseVisualStyleBackColor = true;
+            this.button_ekle.Click += new System.EventHandler(this.button_ekle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(372, 181);
+            this.Controls.Add(this.button_ekle);
+            this.Controls.Add(this.textBox_sözlü2);
+            this.Controls.Add(this.textBox_sözlü1);
+            this.Controls.Add(this.textBox_yazılı2);
+            this.Controls.Add(this.textBox_yazılı1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_adSoy);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_adSoy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_yazılı1;
+        private System.Windows.Forms.TextBox textBox_yazılı2;
+        private System.Windows.Forms.TextBox textBox_sözlü2;
+        private System.Windows.Forms.TextBox textBox_sözlü1;
+        private System.Windows.Forms.Button button_ekle;
     }
 }
 
--- U4_Uyg6/Form1.cs
+++ U4_Uyg6/Form1.cs
@@ -16,5 +16,17 @@ namespace U4_Uyg6
         {
             InitializeComponent();
         }
+        string[] isimler = new string[3];
+        int[,] notlar = new int[3, 4];
+        int index = 0;
+        private void button_ekle_Click(object sender, EventArgs e)
+        {
+            isimler[index] = textBox_adSoy.Text;
+            notlar[index,0] = int.Parse(textBox_yazılı1.Text);
+            notlar[index,1] = int.Parse(textBox_yazılı2.Text);
+            notlar[index,2] = int.Parse(textBox_sözlü1.Text);
+            notlar[index,3] = int.Parse(textBox_sözlü2.Text);
+            index++;
+        }
     }
 }
