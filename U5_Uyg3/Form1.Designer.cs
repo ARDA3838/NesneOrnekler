02f0294f97df53ccdd37d51ed6789ebe778c1623
BIL-LAB1PC03\Ogrenci <123@123.com>
2023-03-14 16:14:31 +0300

    no message

---
+++ U5_Uyg3/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿
+namespace U5_Uyg3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.r5 = new System.Windows.Forms.RadioButton();
+            this.I3 = new System.Windows.Forms.RadioButton();
+            this.I5 = new System.Windows.Forms.RadioButton();
+            this.I7 = new System.Windows.Forms.RadioButton();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.r3 = new System.Windows.Forms.RadioButton();
+            this.gb16 = new System.Windows.Forms.RadioButton();
+            this.gb8 = new System.Windows.Forms.RadioButton();
+            this.gb4 = new System.Windows.Forms.RadioButton();
+            this.tb1 = new System.Windows.Forms.RadioButton();
+            this.gb500 = new System.Windows.Forms.RadioButton();
+            this.gb350 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.btn_Hesapla = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox1.Controls.Add(this.r3);
+            this.groupBox1.Controls.Add(this.I7);
+            this.groupBox1.Controls.Add(this.r5);
+            this.groupBox1.Controls.Add(this.I5);
+            this.groupBox1.Controls.Add(this.I3);
+            this.groupBox1.Location = new System.Drawing.Point(28, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(125, 156);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "işlemciler";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox2.Controls.Add(this.gb4);
+            this.groupBox2.Controls.Add(this.gb8);
+            this.groupBox2.Controls.Add(this.gb16);
+            this.groupBox2.Location = new System.Drawing.Point(195, 69);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(115, 116);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ram bellek";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox3.Controls.Add(this.tb1);
+            this.groupBox3.Controls.Add(this.gb500);
+            this.groupBox3.Controls.Add(this.gb350);
+            this.groupBox3.Location = new System.Drawing.Point(347, 145);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(117, 116);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "sabit disk";
+            // 
+            // r5
+            // 
+            this.r5.AutoSize = true;
+            this.r5.Location = new System.Drawing.Point(16, 99);
+            this.r5.Name = "r5";
+            this.r5.Size = new System.Drawing.Size(82, 17);
+            this.r5.TabIndex = 3;
+            this.r5.TabStop = true;
+            this.r5.Text = "amd ryzen 5";
+            this.r5.UseVisualStyleBackColor = true;
+            // 
+            // I3
+            // 
+            this.I3.AutoSize = true;
+            this.I3.Location = new System.Drawing.Point(16, 76);
+            this.I3.Name = "I3";
+            this.I3.Size = new System.Drawing.Size(79, 17);
+            this.I3.TabIndex = 2;
+            this.I3.TabStop = true;
+            this.I3.Text = "intel core i3";
+            this.I3.UseVisualStyleBackColor = true;
+            // 
+            // I5
+            // 
+            this.I5.AutoSize = true;
+            this.I5.Location = new System.Drawing.Point(16, 53);
+            this.I5.Name = "I5";
+            this.I5.Size = new System.Drawing.Size(79, 17);
+            this.I5.TabIndex = 1;
+            this.I5.TabStop = true;
+            this.I5.Text = "intel core i5";
+            this.I5.UseVisualStyleBackColor = true;
+            // 
+            // I7
+            // 
+            this.I7.AutoSize = true;
+            this.I7.Location = new System.Drawing.Point(16, 30);
+            this.I7.Name = "I7";
+            this.I7.Size = new System.Drawing.Size(82, 17);
+            this.I7.TabIndex = 0;
+            this.I7.TabStop = true;
+            this.I7.Text = " intel core i7";
+            this.I7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.groupBox4.Controls.Add(this.checkBox3);
+            this.groupBox4.Controls.Add(this.checkBox2);
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Location = new System.Drawing.Point(28, 214);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(125, 100);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "ek donanım";
+            // 
+            // r3
+            // 
+            this.r3.AutoSize = true;
+            this.r3.Location = new System.Drawing.Point(16, 122);
+            this.r3.Name = "r3";
+            this.r3.Size = new System.Drawing.Size(82, 17);
+            this.r3.TabIndex = 4;
+            this.r3.TabStop = true;
+            this.r3.Text = "amd ryzen 3";
+            this.r3.UseVisualStyleBackColor = true;
+            // 
+            // gb16
+            // 
+            this.gb16.AutoSize = true;
+            this.gb16.Location = new System.Drawing.Point(15, 29);
+            this.gb16.Name = "gb16";
+            this.gb16.Size = new System.Drawing.Size(49, 17);
+            this.gb16.TabIndex = 0;
+            this.gb16.TabStop = true;
+            this.gb16.Text = "16gb";
+            this.gb16.UseVisualStyleBackColor = true;
+            // 
+            // gb8
+            // 
+            this.gb8.AutoSize = true;
+            this.gb8.Location = new System.Drawing.Point(15, 53);
+            this.gb8.Name = "gb8";
+            this.gb8.Size = new System.Drawing.Size(43, 17);
+            this.gb8.TabIndex = 1;
+            this.gb8.TabStop = true;
+            this.gb8.Text = "8gb";
+            this.gb8.UseVisualStyleBackColor = true;
+            // 
+            // gb4
+            // 
+            this.gb4.AutoSize = true;
+            this.gb4.Location = new System.Drawing.Point(15, 76);
+            this.gb4.Name = "gb4";
+            this.gb4.Size = new System.Drawing.Size(43, 17);
+            this.gb4.TabIndex = 2;
+            this.gb4.TabStop = true;
+            this.gb4.Text = "4gb";
+            this.gb4.UseVisualStyleBackColor = true;
+            // 
+            // tb1
+            // 
+            this.tb1.AutoSize = true;
+            this.tb1.Location = new System.Drawing.Point(17, 30);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(40, 17);
+            this.tb1.TabIndex = 4;
+            this.tb1.TabStop = true;
+            this.tb1.Text = "1tb";
+            this.tb1.UseVisualStyleBackColor = true;
+            // 
+            // gb500
+            // 
+            this.gb500.AutoSize = true;
+            this.gb500.Location = new System.Drawing.Point(17, 53);
+            this.gb500.Name = "gb500";
+            this.gb500.Size = new System.Drawing.Size(55, 17);
+            this.gb500.TabIndex = 5;
+            this.gb500.TabStop = true;
+            this.gb500.Text = "500gb";
+            this.gb500.UseVisualStyleBackColor = true;
+            // 
+            // gb350
+            // 
+            this.gb350.AutoSize = true;
+            this.gb350.Location = new System.Drawing.Point(17, 76);
+            this.gb350.Name = "gb350";
+            this.gb350.Size = new System.Drawing.Size(55, 17);
+            this.gb350.TabIndex = 6;
+            this.gb350.TabStop = true;
+            this.gb350.Text = "350gb";
+            this.gb350.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 51);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 74);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // btn_Hesapla
+            // 
+            this.btn_Hesapla.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_Hesapla.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Hesapla.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Hesapla.Location = new System.Drawing.Point(195, 237);
+            this.btn_Hesapla.Name = "btn_Hesapla";
+            this.btn_Hesapla.Size = new System.Drawing.Size(115, 58);
+            this.btn_Hesapla.TabIndex = 4;
+            this.btn_Hesapla.Text = "HESAPLA";
+            this.btn_Hesapla.UseVisualStyleBackColor = false;
+            this.btn_Hesapla.Click += new System.EventHandler(this.btn_Hesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(558, 388);
+            this.Controls.Add(this.btn_Hesapla);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton r5;
+        private System.Windows.Forms.RadioButton I3;
+        private System.Windows.Forms.RadioButton I5;
+        private System.Windows.Forms.RadioButton I7;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RadioButton r3;
+        private System.Windows.Forms.RadioButton gb4;
+        private System.Windows.Forms.RadioButton gb8;
+        private System.Windows.Forms.RadioButton gb16;
+        private System.Windows.Forms.RadioButton tb1;
+        private System.Windows.Forms.RadioButton gb500;
+        private System.Windows.Forms.RadioButton gb350;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btn_Hesapla;
+    }
+}
+
---
+++ U5_Uyg3/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U5_Uyg3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_Hesapla_Click(object sender, EventArgs e)
+        {
+            decimal TabanFiyat = 500;
+
+            decimal cpuFiyat = 0;
+
+            if (I7.Checked)
+                cpuFiyat = 790;
+            else if (I5.Checked)
+                cpuFiyat = 520;
+            else if (I3.Checked)
+                cpuFiyat = 200;
+            else if (r5.Checked)
+                cpuFiyat = 960;
+            else if (r3.Checked) ;
+            cpuFiyat = 440;
+            TabanFiyat += cpuFiyat;
+
+            decimal RamFiyat -0;
+            if (gb16.Checked)
+                RamFiyat = 500;
+            if else (gb8.Checked)
+                RamFiyat = 280;
+            if else (gb4.Checked)
+                RamFiyat = 160;
+            TabanFiyat += RamFiyat;
+
+            MessageBox.Show(string.Format("toplam fiyat ={0:C}", TabanFiyat));
+            
+        }
+    }
+}
