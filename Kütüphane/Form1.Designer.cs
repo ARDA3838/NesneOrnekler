064f717b6375a506ba6fb3aaa2e8efbb58912528
BIL-LAB1PC01\Ogrenci <123@123.COM>
2023-05-09 16:49:35 +0300

    no message

--- Kütüphane/Form1.Designer.cs
+++ Kütüphane/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Kütüphane
 {
-    partial class Form1
+    partial class formAnaSayfa
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,88 @@ namespace Kütüphane
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAnaSayfa));
+            this.btnKitap = new System.Windows.Forms.Button();
+            this.btnOgrenci = new System.Windows.Forms.Button();
+            this.btnTur = new System.Windows.Forms.Button();
+            this.btnOdunc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKitap
+            // 
+            this.btnKitap.Image = ((System.Drawing.Image)(resources.GetObject("btnKitap.Image")));
+            this.btnKitap.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnKitap.Location = new System.Drawing.Point(21, 29);
+            this.btnKitap.Name = "btnKitap";
+            this.btnKitap.Size = new System.Drawing.Size(170, 75);
+            this.btnKitap.TabIndex = 0;
+            this.btnKitap.Text = "Kitap İşlemleri";
+            this.btnKitap.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnKitap.UseVisualStyleBackColor = true;
+            this.btnKitap.Click += new System.EventHandler(this.btnKitap_Click);
+            // 
+            // btnOgrenci
+            // 
+            this.btnOgrenci.Image = ((System.Drawing.Image)(resources.GetObject("btnOgrenci.Image")));
+            this.btnOgrenci.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOgrenci.Location = new System.Drawing.Point(241, 29);
+            this.btnOgrenci.Name = "btnOgrenci";
+            this.btnOgrenci.Size = new System.Drawing.Size(170, 75);
+            this.btnOgrenci.TabIndex = 1;
+            this.btnOgrenci.Text = "Öğrenci İşlemleri";
+            this.btnOgrenci.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOgrenci.UseVisualStyleBackColor = true;
+            this.btnOgrenci.Click += new System.EventHandler(this.btnOgrenci_Click);
+            // 
+            // btnTur
+            // 
+            this.btnTur.Image = ((System.Drawing.Image)(resources.GetObject("btnTur.Image")));
+            this.btnTur.Location = new System.Drawing.Point(21, 153);
+            this.btnTur.Name = "btnTur";
+            this.btnTur.Size = new System.Drawing.Size(170, 75);
+            this.btnTur.TabIndex = 2;
+            this.btnTur.Text = "Tür İşlemleri";
+            this.btnTur.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnTur.UseVisualStyleBackColor = true;
+            this.btnTur.Click += new System.EventHandler(this.btnTur_Click);
+            // 
+            // btnOdunc
+            // 
+            this.btnOdunc.Image = ((System.Drawing.Image)(resources.GetObject("btnOdunc.Image")));
+            this.btnOdunc.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnOdunc.Location = new System.Drawing.Point(241, 153);
+            this.btnOdunc.Name = "btnOdunc";
+            this.btnOdunc.Size = new System.Drawing.Size(170, 75);
+            this.btnOdunc.TabIndex = 3;
+            this.btnOdunc.Text = "Ödünç Kitap İşlemleri";
+            this.btnOdunc.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnOdunc.UseVisualStyleBackColor = true;
+            this.btnOdunc.Click += new System.EventHandler(this.btnOdunc_Click);
+            // 
+            // formAnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.Controls.Add(this.btnOdunc);
+            this.Controls.Add(this.btnTur);
+            this.Controls.Add(this.btnOgrenci);
+            this.Controls.Add(this.btnKitap);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "formAnaSayfa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kütüphane Prajesi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnKitap;
+        private System.Windows.Forms.Button btnOgrenci;
+        private System.Windows.Forms.Button btnTur;
+        private System.Windows.Forms.Button btnOdunc;
     }
 }
 
--- Kütüphane/Form1.cs
+++ Kütüphane/Form1.cs
@@ -10,11 +10,40 @@ using System.Windows.Forms;
 
 namespace Kütüphane
 {
-    public partial class Form1 : Form
+    public partial class formAnaSayfa : Form
     {
-        public Form1()
+        public formAnaSayfa()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKitap_Click(object sender, EventArgs e)
+        {
+            Kitapislemleri kitap = new Kitapislemleri();
+            kitap.ShowDialog();
+        }
+
+        private void btnOgrenci_Click(object sender, EventArgs e)
+        {
+            Ogrenciislemleri ogrenci = new Ogrenciislemleri();
+            ogrenci.ShowDialog();
+        }
+
+        private void btnTur_Click(object sender, EventArgs e)
+        {
+            Turislemeleri kitapturu = new Turislemeleri();
+            kitapturu.ShowDialog();
+        }
+
+        private void btnOdunc_Click(object sender, EventArgs e)
+        {
+            OduncKitapislemleri odunc = new OduncKitapislemleri();
+            odunc.ShowDialog();
         }
     }
 }
